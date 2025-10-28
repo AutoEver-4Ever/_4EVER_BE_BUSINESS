d0c3d3b2094d4e847fd872ca100b6fddc09295d1
Hojip-Kim <rlaghwlq12@naver.com>
2025-10-28 16:22:15 +0900

    #12 feat : 데이터 적재를 위한 SD 관련 로직 추가

--- src/main/java/org/ever/_4ever_be_business/order/entity/OrderStatus.java
+++ src/main/java/org/ever/_4ever_be_business/order/entity/OrderStatus.java
@@ -1,31 +1,11 @@
 package org.ever._4ever_be_business.order.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.ever._4ever_be_business.common.entity.TimeStamp;
-import org.ever._4ever_be_business.common.util.UuidV7Generator;
-
-@Entity
-@Table(name = "order_status")
-@NoArgsConstructor
-@Getter
-public class OrderStatus extends TimeStamp {
-
-    @Id
-    @Column(length = 36)
-    private String id;
-
-    private String status;
-
-    public OrderStatus(String status) {
-        this.status = status;
-    }
-
-    @PrePersist
-    public void generateId() {
-        if (this.id == null) {
-            this.id = UuidV7Generator.generate();
-        }
-    }
+public enum OrderStatus {
+    PENDING,              // 대기
+    CONFIRMED,            // 확정
+    IN_PROGRESS,          // 진행중
+    READY_FOR_SHIPMENT,   // 출고 준비
+    SHIPPED,              // 출고 완료
+    DELIVERED,            // 배송 완료
+    CANCELLED             // 취소
 }
