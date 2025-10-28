cec91d59e3e1c2283d3a4dbefe82777842a8aefb
Gumraze-git <galaxydh4110@gmail.com>
2025-10-28 21:55:33 +0900

    refac(hrm): 사용되지 않는 repository 제거 (#12)
    - 내부 사용자 저장소 관련 코드 삭제

--- src/main/java/org/ever/_4ever_be_business/hr/repository/InternalUserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ever._4ever_be_business.hr.repository;
-
-import org.ever._4ever_be_business.hr.entity.InternelUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InternalUserRepository extends JpaRepository<InternelUser, String> {
-}
