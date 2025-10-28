e8d370b76ead53a9806abb2bee7a143e99e1c602
Hojip-Kim <rlaghwlq12@naver.com>
2025-10-28 16:21:37 +0900

    #12 feat : 데이터 적재를 위한 AR & AP 관련 로직 추가

--- src/main/java/org/ever/_4ever_be_business/Application.java
+++ src/main/java/org/ever/_4ever_be_business/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = {
     MongoAutoConfiguration.class,
     MongoDataAutoConfiguration.class
