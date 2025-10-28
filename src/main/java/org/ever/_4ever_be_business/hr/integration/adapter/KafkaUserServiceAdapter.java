ce1904988ab7985f84e045dca518e8027e20c8a5
Gumraze-git <galaxydh4110@gmail.com>
2025-10-28 22:08:33 +0900

    feat(hrm): 카프카를 지원하는 클래스 추가 (#12)
    - `KafkaUserServiceAdapter` 클래스 생성 및 사용자 서비스 포트 구현
    - `MockUserServiceAdapter`에 `createInternalUserAccount` 메서드 추가

--- src/main/java/org/ever/_4ever_be_business/hr/integration/adapter/KafkaUserServiceAdapter.java
+++ src/main/java/org/ever/_4ever_be_business/hr/integration/adapter/KafkaUserServiceAdapter.java
@@ -2,38 +2,33 @@ package org.ever._4ever_be_business.hr.integration.adapter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ever._4ever_be_business.common.mock.MockDataProvider;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.ever._4ever_be_business.hr.dto.request.AuthUserCreateRequestDto;
 import org.ever._4ever_be_business.hr.dto.request.AuthUserCreateResponseDto;
 import org.ever._4ever_be_business.hr.dto.response.UserInfoResponse;
 import org.ever._4ever_be_business.hr.integration.port.UserServicePort;
+import org.ever._4ever_be_business.infrastructure.kafka.producer.KafkaProducerService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-/**
- * UserServicePort의 Mock 구현체
- * dev 환경에서 사용
- */
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@ConditionalOnProperty(name = "external.mock.enabled", havingValue = "true", matchIfMissing = true)
-public class MockUserServiceAdapter implements UserServicePort {
+@ConditionalOnProperty(name = "external.mock.enabled", havingValue = "kafka", matchIfMissing = true)
+public class KafkaUserServiceAdapter implements UserServicePort {
 
-    private final MockDataProvider mockDataProvider;
+    private final KafkaProducerService kafkaProducerService;
 
     @Override
     public CompletableFuture<UserInfoResponse> getMultipleUserInfo(List<Long> internelUserIds) {
-        log.info("[MOCK ADAPTER] getMultipleUserInfo 호출 - internelUserIds: {}", internelUserIds);
-        return CompletableFuture.completedFuture(mockDataProvider.createMockUserInfo(internelUserIds));
+        return null;
     }
 
     @Override
     public CompletableFuture<AuthUserCreateResponseDto> createInternalUserAccount(AuthUserCreateRequestDto request) {
-        log.info("[MOCK ADAPTER] AuthUserCreateResponseDto 호출");
         return null;
     }
 }
