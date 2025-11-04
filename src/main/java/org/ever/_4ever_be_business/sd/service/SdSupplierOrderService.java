ef913c3f1f6363a0d9df3c7eaea0a285aca929f4
Gumraze-git <galaxydh4110@gmail.com>
2025-11-04 21:23:50 +0900

    refac(sd): 대시보드용 공급사 주문 목록 dto 도입 및 관련 서비스 업데이트 (#39)
    - `SupplierOrderWorkflowItemDto` 추가 및 `SalesOrderListItemDto` 대체
    - 서비스 및 컨트롤러에서 DTO 변경에 따른 로직 수정
    - 비어 있는 페이지 반환 로직 유지

--- src/main/java/org/ever/_4ever_be_business/sd/service/SdSupplierOrderService.java
+++ src/main/java/org/ever/_4ever_be_business/sd/service/SdSupplierOrderService.java
@@ -1,6 +1,6 @@
 package org.ever._4ever_be_business.sd.service;
 
-import org.ever._4ever_be_business.sd.dto.response.SalesOrderListItemDto;
+import org.ever._4ever_be_business.sd.dto.response.SupplierOrderWorkflowItemDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,6 +8,5 @@ import org.springframework.data.domain.Pageable;
  * 공급사 사용자 기준 주문서 목록 조회 (대시보드용)
  */
 public interface SdSupplierOrderService {
-    Page<SalesOrderListItemDto> getSupplierOrderList(String supplierUserId, Pageable pageable);
+    Page<SupplierOrderWorkflowItemDto> getSupplierOrderList(String supplierUserId, Pageable pageable);
 }
-
--- src/main/java/org/ever/_4ever_be_business/sd/service/impl/SdSupplierOrderServiceImpl.java
+++ src/main/java/org/ever/_4ever_be_business/sd/service/impl/SdSupplierOrderServiceImpl.java
@@ -2,7 +2,7 @@ package org.ever._4ever_be_business.sd.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ever._4ever_be_business.sd.dto.response.SalesOrderListItemDto;
+import org.ever._4ever_be_business.sd.dto.response.SupplierOrderWorkflowItemDto;
 import org.ever._4ever_be_business.sd.service.SdSupplierOrderService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -17,11 +17,10 @@ import java.util.List;
 public class SdSupplierOrderServiceImpl implements SdSupplierOrderService {
 
     @Override
-    public Page<SalesOrderListItemDto> getSupplierOrderList(String supplierUserId, Pageable pageable) {
-        // 1단계: DAO 없이 우선 스켈레톤 반환 (추후 Repository 연동)
+    public Page<SupplierOrderWorkflowItemDto> getSupplierOrderList(String supplierUserId, Pageable pageable) {
+        // 1단계: Repository 연동 전까지 빈 페이지 반환
         log.info("[BUSINESS][SD] 대시보드 공급사 주문서 목록 조회 - userId: {}, page: {}, size: {}",
                 supplierUserId, pageable.getPageNumber(), pageable.getPageSize());
         return new PageImpl<>(List.of(), pageable, 0);
     }
 }
-
